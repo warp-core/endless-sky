d70254dbecca74d5a6009e1c64bea75f607ac409
quyykk <quyykk@protonmail.com>
2022-08-23 19:03:50 +0200

    Add missing #include.

--- source/WeightedList.h
+++ source/WeightedList.h
@@ -16,6 +16,7 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #include "Random.h"
 
 #include <cstddef>
+#include <functional>
 #include <iterator>
 #include <numeric>
 #include <stdexcept>

279caa662679da33ac84fe66f7df7607941eb205
warp-core <warp-core@users.noreply.github.com>
2024-12-04 19:59:40 +0000

    Unfix

--- source/GameData.cpp
+++ source/GameData.cpp
@@ -326,8 +326,6 @@ double GameData::GetProgress()
 	double spriteProgress = static_cast<double>(spriteLoadingProgress) / totalSprites;
 	Logger::LogError("Sprite loading progress: " + to_string(spriteLoadingProgress));
 	Logger::LogError("Sprite progress: " + to_string(spriteProgress));
-	if(!totalSprites)
-		spriteProgress = 0.;
 	return min({spriteProgress, Audio::GetProgress(), objects.GetProgress()});
 }
 
